fc856fa983c32db33c86c252fce9e32ce9488bf7
zinine73 <zinine@naver.com>
2025-01-17 11:12:12 +0900

    player

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}

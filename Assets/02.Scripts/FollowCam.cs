089c568bb2edb15a5559d4aec4fd9144e69afef6
zinine73 <zinine@naver.com>
2025-01-27 11:47:18 +0900

    smoothdamp

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -15,6 +15,13 @@ public class FollowCam : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
 
+    // 반응속도
+    public float damping = 10.0f;
+    // camera offset
+    public float targetOffset = 2.0f;
+    // smoothdamp
+    private Vector3 velocity = Vector3.zero;
+
     void Start()
     {
         camTr = GetComponent<Transform>();
@@ -23,10 +30,19 @@ public class FollowCam : MonoBehaviour
     void LateUpdate()
     {
         // 추적할 대상 뒤쪽으로 distance만큼, 높이는 height만큼 이동
-        camTr.position = tragetTr.position
+        Vector3 pos = tragetTr.position
                         + (-tragetTr.forward * distance)
                         + (Vector3.up * height);
+        // 구면선형보간 함수를 이용해서 부드럽게 위치 변경
+        //camTr.position = Vector3.Slerp(camTr.position,          // 시작 위치
+        //                            pos,                        // 목표 위치
+        //                            Time.deltaTime * damping);  // 시간
+        // smoothdamp
+        camTr.position = Vector3.SmoothDamp(camTr.position, // 시작 위치
+                                            pos,            // 목표 위치
+                                            ref velocity,   // 현재 속도
+                                            damping);       // 도달 시간
         // 피벗좌표를 향해 회전
-        camTr.LookAt(tragetTr.position);
+        camTr.LookAt(tragetTr.position + (tragetTr.up * targetOffset));
     }
 }

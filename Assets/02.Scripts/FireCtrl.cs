54616e879271dfcf407e5efbd457ffa0bcfa55e5
zinine73 <zinine@naver.com>
2025-02-11 11:50:09 +0900

    audio

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 삭제 안되도록 하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     // 총알 프리펩
     public GameObject bullet;
     // 총알 발사 좌표
     public Transform firePos;
+    // 총소리 음원
+    public AudioClip fireSfx;
+
+    // AudioSource Component
+    private new AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();    
+    }
 
     void Update()
     {
@@ -22,5 +34,8 @@ public class FireCtrl : MonoBehaviour
     {
         // bullet 프리펩을 동적으로 생성
         Instantiate(bullet, firePos.position, firePos.rotation);
+
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
